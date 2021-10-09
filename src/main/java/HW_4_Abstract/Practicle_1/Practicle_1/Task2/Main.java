29c7cd0f2f85b6f37785af94feed6cdd7116b473
Pavel Garmatyuk <m-krum@yandex.ru>
2021-10-09 21:24:42 +0300

    Java Essential: 3 lesson homework task#2 is done

---
+++ src/main/java/HW_4_Abstract/Practicle_1/Practicle_1/Task2/Main.java
@@ -0,0 +1,17 @@
+package HW_4_Abstract.Practicle_1.Practicle_1.Task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Car bmw = new Car();
+        bmw.year = 2020;
+
+        Car audi = new Car();
+        audi.color = "yellow";
+
+        Car lada = new Car();
+
+        Car lexus = new Car();
+        lexus.color = "red";
+        lexus.year = 2021;
+    }
+}
